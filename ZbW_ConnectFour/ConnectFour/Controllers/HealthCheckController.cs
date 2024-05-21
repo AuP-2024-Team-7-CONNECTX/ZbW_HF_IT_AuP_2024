7cc9eca037e84b67e9a4f101c334b1501211b9bf
Nick Ponnadu <nick.ponnadu@gmx.ch>
2024-05-21 21:31:47 +0200

    test numero hunderto

--- ZbW_ConnectFour/ConnectFour/Controllers/HealthCheckController.cs
+++ ZbW_ConnectFour/ConnectFour/Controllers/HealthCheckController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ namespace ConnectFour.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+[EnableCors("AllowAll")]
 public class HealthCheckController : ControllerBase
 {
 	private readonly HealthCheckService _healthCheckService;
@@ -19,11 +21,11 @@ public class HealthCheckController : ControllerBase
 	[HttpGet]
 	public async Task<IActionResult> Get()
 	{
-		//// Setze CORS-Header in der Antwort
-		Response.Headers.Add("Access-Control-Allow-Origin", "*"); // Erlaubt den Zugriff von allen Herkunftsorten
-																  ////Response.Headers.Add("Access-Control-Allow-Methods", "GET"); // Erlaubt POST-Anfragen
-																  //Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type"); // Erlaubt bestimmte Header
-																  //Response.Headers.Add("Access-Control-Allow-Credentials", "true"); // Erlaubt bestimmte Header
+		////// Setze CORS-Header in der Antwort
+		//Response.Headers.Add("Access-Control-Allow-Origin", "*"); // Erlaubt den Zugriff von allen Herkunftsorten
+		//														  ////Response.Headers.Add("Access-Control-Allow-Methods", "GET"); // Erlaubt POST-Anfragen
+		//														  //Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type"); // Erlaubt bestimmte Header
+		//														  //Response.Headers.Add("Access-Control-Allow-Credentials", "true"); // Erlaubt bestimmte Header
 
 
 		return Ok("test Text 123");
