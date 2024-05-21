6f2f1d2855671eabfc0678138e5b41587b06d991
Nick Ponnadu <nick.ponnadu@gmx.ch>
2024-05-21 19:52:15 +0200

    mon3y endpoint

--- Frontend/config.js
+++ Frontend/config.js
@@ -1,2 +1,3 @@
-const endpoint = "https://100.87.201.117:5000";
-// const endpoint = "https://localhost:5000";
+// const endpoint = "https://100.87.201.117:5000/api";
+// const endpoint = "https://localhost:5000/api";
+const endpoint = "https://connectx.mon3y.ch/api";
