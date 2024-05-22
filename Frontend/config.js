56c84912bc2ed054a1af7d6a7cda00b6ed585dca
Nick Ponnadu <nick.ponnadu@gmx.ch>
2024-05-22 21:00:20 +0200

    money end

--- Frontend/config.js
+++ Frontend/config.js
@@ -1,3 +1,3 @@
-const endpoint = "https://100.87.201.117:5000";
+// const endpoint = "https://100.87.201.117:5000";
 // const endpoint = "https://localhost:5000";
-// const endpoint = "https://connectx.mon3y.ch:5000";
+const endpoint = "https://connectx.mon3y.ch:5000";
